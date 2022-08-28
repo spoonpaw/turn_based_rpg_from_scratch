b1a677787fb9b35add90045fbdda3cf7ca9042c8
Evan Norton <evan@retro-mmo.com>
2022-08-28 19:00:39 -0400

    order

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const express = require("express");
 const { resolve } = require("path");
 
 const app = express();
+
+app.use("/", express.static(resolve("_build")));
+
 const server = new Server(app);
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 server.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
-
-app.use("/", express.static(resolve("_build")));
