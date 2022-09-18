6e9da360c2993a8e2056eddc6bd59655bc245777
alex <alexanderstclair@zoho.com>
2022-09-17 19:15:39 -0700

    battle scene overhaul

--- src/classes/Enemy.ts
+++ src/classes/Enemy.ts
@@ -1,8 +1,23 @@
 import Unit from './Unit';
+import eventsCenter from '../utils/EventsCenter';
 
 export default class Enemy extends Unit {
-    constructor(scene, x, y, texture, frame, type, hp, damage) {
-        super(scene, x, y, texture, frame, type, hp, damage);
+
+    constructor(scene, x, y, texture, frame, type, hp, damage, initiative) {
+        super(scene, x, y, texture, frame, type, hp, damage, initiative);
         this.setScale(6);
+        this.setInteractive();
+
+        this.on('pointerdown', () => {
+            // check if the battle scene is ready for player interaction at all
+
+            if (scene.interactionState !== 'attack') {
+                return;
+            }
+
+            eventsCenter.emit('actionSelect', {
+                action: 'attack', target: this
+            });
+        });
     }
 }
